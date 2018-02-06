3a828d7253928dbde554ea62ea1f5686dfb9a1bd
huobb0 <zsombor.kovacs@gmail.com>
2018-02-06 22:32:20 +0100

    Initial Commit

---
+++ SqliteStorageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteStorageDemo
+//
+//  Created by Zsombor on 2018. 02. 06..
+//  Copyright © 2018. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliteStorageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SqliteStorageDemo
+//
+//  Created by Zsombor on 2018. 02. 06..
+//  Copyright © 2018. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
